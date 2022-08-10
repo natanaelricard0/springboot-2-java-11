eb030b99d1bb46a94ebd5afd66f0c424f489afe4
Natanael Ricardo <nathanshurle@gmail.com>
2022-08-10 11:26:53 -0300

    order, instant, iso 8601 - PART I

--- src/main/java/com/kolda/course/entities/User1.java
+++ src/main/java/com/kolda/course/entities/User1.java
@@ -1,14 +1,19 @@
 package com.kolda.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User1 implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,9 @@ public class User1 implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User1() {
 	}
@@ -71,6 +79,10 @@ public class User1 implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -88,6 +100,4 @@ public class User1 implements Serializable {
 		User1 other = (User1) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 }
