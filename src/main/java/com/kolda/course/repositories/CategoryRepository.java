9f2095b763d16ddd27fc666ad861c42a0df286e9
Natanael Ricardo <nathanshurle@gmail.com>
2022-08-11 11:38:46 -0300

    categorie

---
+++ src/main/java/com/kolda/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.kolda.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kolda.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
